b3db91d98840aca2ce27cfd22ed6092cf9353665
Moe Khan <mobashir16@gmail.com>
2025-05-25 21:14:48 +0530

    Swap without temp

---
+++ swap_two_numbers_without_temp/swap.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 20
+print('num1', num1, 'num2', num2)
+
+num1 = num1 + num2
+print('num1', num1, 'num2', num2)
+
+num2 = num1 - num2
+print('num1', num1, 'num2', num2)
+
+num1 = num1 - num2
+print('num1', num1, 'num2', num2)
